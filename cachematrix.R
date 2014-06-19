a39c66ce512c8ce3af0147524d4fadd41fb23cc4
ElenaCB <rimay79@gmail.com>
2014-06-19 13:26:21 -0700

    cachematrix with the function that creates a special 'matrix'.

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,21 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## This function creates a special "matrix" object that can cache
+## its inverse. 
 
+makeCacheMatrix <- function(x = matrix()) {
+             m<-NULL
+             set<-function(y){
+                        x<<-y
+                        m<<-NULL
+             }
+             get<-function()x
+             setinverse<-function(solve) m<<-solve
+             getinverse<-function() m
+             list (set=set, get=get, 
+                   setinverse=setinverse,
+                   getinverse=getinverse)
 }
 
 
